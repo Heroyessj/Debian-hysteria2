31561432098cbfe1a8c28f90cd1bb5d05b2eb9bc
Heroyessj <153788917+Heroyessj@users.noreply.github.com>
2026-02-06 19:51:09 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ chown hysteria /etc/hysteria/server.crt
 
 echo "=== 写入 HY2 配置 ==="
 cat << EOF > /etc/hysteria/config.yaml
-listen: :45433
+listen: :12201
 
 tls:
   cert: /etc/hysteria/server.crt
